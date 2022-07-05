b7f58df62f3707ebdbaeaeea1f97259c8d4398bd
[Manish Kunwar] <manish_kunwar@hotmail.com>
2022-07-05 20:56:30 +0545

    secret key updated

--- wisdompets/wisdompets/settings.py
+++ wisdompets/wisdompets/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o2rvs@)9hx+v8*^_bnp@av!1p*%=r4s&a^^1g4%*9(l(sg1o!a'
+SECRET_KEY = config("SECRET_KEY") # this is to replace the secret key you cut away before 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
