84797a4ba561190f4f115b4714aa812177461cbf
[Manish Kunwar] <manish_kunwar@hotmail.com>
2022-07-06 13:55:40 +0545

    django folder added

---
+++ django/firstapp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from .views import add, div, index, mul, sub
+
+urlpatterns = [
+    path("", index, name='index_page'),
+    path('add/', add, name='add_page'),
+    path('sub/', sub, name='sub_page'),
+    path('mul/', mul, name='mul_page'),
+    path('div/', div, name='div_page')
+]
